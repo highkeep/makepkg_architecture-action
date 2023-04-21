d4688010b81fdeb3ce577a6f2d277299109caa67
Nick Marshall <nmarshall@highkeep.com>
2023-04-21 16:55:54 -0700

    init

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euo pipefail
+
+# Added builder as seen in edlanglois/pkgbuild-action
+# mainly for permissions
+useradd builder -m
+# When installing dependencies, makepkg will use sudo
+# Give user `builder` passwordless sudo access
+echo "builder ALL=(ALL) NOPASSWD: ALL" >>/etc/sudoers
+
+# Give all users (particularly builder) full access to these files
+chmod -R a+rw .
+
+echo "${INPUT_ARCHITECTURE}"
+echo "${INPUT_TPLMAKEPKGCONF}"
+echo "${INPUT_TPLPACMANCONF}"
