db8140440dad3ce30f7bb572875b25675434dd08
Nick Marshall <nmarshall@highkeep.com>
2023-04-21 19:16:54 -0700

    tidy up

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,7 @@ echo "builder ALL=(ALL) NOPASSWD: ALL" >>/etc/sudoers
 # Give all users (particularly builder) full access to these files
 chmod -R a+rw .
 
+# Set up sudo cmd to make like a little easier
 sudoCMD="sudo -u builder"
 
 function setMarch() {
