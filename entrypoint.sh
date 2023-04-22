d452d2967daa53c5fde68de1733672fc0e46ee24
Nick Marshall <nmarshall@highkeep.com>
2023-04-21 18:19:44 -0700

    built entrypoint and updated actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,8 +11,75 @@ echo "builder ALL=(ALL) NOPASSWD: ALL" >>/etc/sudoers
 # Give all users (particularly builder) full access to these files
 chmod -R a+rw .
 
-echo "${INPUT_ARCHITECTURE}"
-echo "${INPUT_TPLMAKEPKGCONF}"
-echo "${INPUT_TPLPACMANCONF}"
-echo "${INPUT_PKG}"
-echo "${INPUT_UPDATEPKG}"
+sudoCMD="sudo -u builder"
+
+function setMarch() {
+    ${sudoCMD} sed -i -r "s/(march=)[A-Za-z0-9-]+(\s?)/\1${1}\2/g" ${2}
+}
+
+function setMtune() {
+    ${sudoCMD} sed -i -r "s/(mtune=)[A-Za-z0-9-]+(\s?)/\1${1}\2/g" ${2}
+}
+
+function setTargetCpu() {
+    ${sudoCMD} sed -i -r "s/(target-cpu=)[A-Za-z0-9-]+(\s?)/\1${1}\2/g" ${2}
+}
+
+# Setup output directory
+${sudoCMD} mkdir "${INPUT_CONFDIR:-tmpConf}"
+
+# Work on makepkg config
+if [ -n "${INPUT_TPLMAKEPKGCONF:-}" ]; then
+    makepkgFile="${INPUT_CONFDIR:-tmpConf}${INPUT_ARCHITECTURE}_makepkg.conf"
+
+    # Copy makepkg template to output directory
+    ${sudoCMD} cp "${INPUT_TPLMAKEPKGCONF}" ${makepkgFile}
+
+    # Update makepkg to use correct architecture
+    if [[ "${INPUT_ARCHITECTURE:-'generic'}" == 'generic' ]]; then
+        setmarch "x86-64" ${makepkgFile}
+        setmtune "generic" ${makepkgFile}
+        settargetcpu "x86-64" ${makepkgFile}
+    else
+        setmarch "${INPUT_ARCHITECTURE}" ${makepkgFile}
+        setmtune "${INPUT_ARCHITECTURE}" ${makepkgFile}
+        settargetcpu "${INPUT_ARCHITECTURE}" ${makepkgFile}
+    fi
+
+    echo "makepkgConf=${makepkgFile}" >>$GITHUB_OUTPUT
+fi
+
+# Work on pacman config
+if [ -n "${INPUT_TPLPACMANCONF:-}" ]; then
+    pacmanFile="${INPUT_CONFDIR:-tmpConf}/${INPUT_ARCHITECTURE}_pacman.conf"
+
+    # Copy pacman template to output directory
+    ${sudoCMD} cp "${INPUT_TPLPACMANCONF}" ${pacmanFile}
+
+    if [ -n "${INPUT_REPOTAGKEY:-}"]; then
+        ${sudoCMD} sed -i "s/${INPUT_REPOTAGKEY}/${INPUT_REPOTAG:-${INPUT_ARCHITECTURE}}/g" ${pacmanFile}
+    fi
+
+    # Assume pacman will be using the gh release repo
+    ghRepoServer="$GITHUB_SERVER_URL"/"$GITHUB_REPOSITORY"/releases/download/"${INPUT_REPOTAG:-}"
+
+    if [ -n "${INPUT_REPOSERVERKEY:-}" ]; then
+        ${sudoCMD} sed -i "s/${INPUT_REPOSERVERKEY}/${INPUT_REPOSERVER:-${ghRepoServer}}/g" ${pacmanFile}
+    fi
+
+    echo "pacmanConf=${pacmanFile}" >>$GITHUB_OUTPUT
+fi
+
+if [[ "${INPUT_UPDATEPKG:-false}" == true ]]; then
+    if [ -n "${INPUT_PKG:-}" ]; then
+        if [[ "${INPUT_ARCHITECTURE:-'generic'}" == 'generic' ]]; then
+            setmarch "x86-64" ${INPUT_PKG}/PKGBUILD
+            setmtune "generic" ${INPUT_PKG}/PKGBUILD
+            settargetcpu "x86-64" ${INPUT_PKG}/PKGBUILD
+        else
+            setmarch "${INPUT_ARCHITECTURE}" ${INPUT_PKG}/PKGBUILD
+            setmtune "${INPUT_ARCHITECTURE}" ${INPUT_PKG}/PKGBUILD
+            settargetcpu "${INPUT_ARCHITECTURE}" ${INPUT_PKG}/PKGBUILD
+        fi
+    fi
+fi
